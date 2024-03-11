04cffb5e1f3e505ad03606a3b09b7caeb54cec4c
Shekhar Tyagi <stya0003@student.monash.edu>
2024-03-11 15:23:20 +1100

    Adding hash routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Password from './components/password';
 import Dashboard from './components/dashboard'; 
 import UVImpact from './components/impact';
