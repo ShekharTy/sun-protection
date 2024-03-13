c3e5d46c43e89a5742d557d2f1f3b300cc59c4ed
Shekhar Tyagi <stya0003@student.monash.edu>
2024-03-13 14:33:48 +1100

    Made dashboard flexible

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -10,10 +10,14 @@ function Dashboard() {
         <div>
             <Header />
             <div className="picture">
+                <div className='image'>
                 <img src={dashboardpage} alt='Dashboard' />
+                </div>
+                <div className='text'>
                 <h1>SunSmart provides a one-stop solution for all your sun protection needs.</h1>
                 <h3>The website offers live UV level updates for your location and detailed information about the protection you need according to the UV levels.<br />It also helps keep track of your family's sunscreen usage.</h3>
                 <button className='uvlevel' onClick={() => navigate('/uvlevel')}>UV Level Finder</button>
+                </div>
             </div>
         </div>
     );
--- src/components/header.js
+++ src/components/header.js
@@ -8,14 +8,16 @@ function Header() {
   return (
     <div className='header'>
         {/*To navigate back to dashboard when user clicks SunSmart */}
-      <h1 onClick={() => navigate('/dashboard')}>Sun Protection Website</h1>
-      <h6>BE SMART STAY SAFE</h6>
+      <h1 onClick={() => navigate('/dashboard')}>Sun Protection</h1><br/>
       {/*Navigate to different pages for each button */}
-      <button className='uvlevel' onClick={() => navigate('/uvlevel')}>UV Level Finder</button>
+      <div className='uvlevel'>
+      <button onClick={() => navigate('/dashboard')}>Home</button>
+      <button onClick={() => navigate('/uvlevel')}>UV Level Finder</button>
       <button onClick={() => navigate('/sunprotect')}>Sun Protection</button>
       <button onClick={() => navigate('/impact')}>UV Impacts</button>
       <button onClick={() => navigate('/sunscreen-usage')}>Sunscreen Usage</button>
       <button onClick={() => navigate('/use-sunscreen')}>Sunscreen Reminder</button>
+      </div>
     </div>
   );
 }
